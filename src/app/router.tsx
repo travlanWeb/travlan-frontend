551eed0f4eb3388726a2136e93090cd20cc4128c
wuheuijeong <wuheuijeong@wuheuijeongui-iMac.local>
2026-09-14 10:25:46 +0900

    feat: 라우터에 홈 화면 연결 + 기존 지도 페이지를 메인 -> map 페이지로 분리

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,41 +1,41 @@
 // createBrowserRouter: 브라우저 주소창(URL)과 컴포넌트 매핑해주는 함수
 
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from 'react-router-dom'
+import Header from '../widgets/header/Header'
+import HomePage from '../pages/home/ui/HomePage'
+import MainPage from '../pages/main/MainPage'
+import TimelinePage from '../pages/timeline/TimelinePage'
+import CommunityPage from '../pages/community/CommunityPage'
+import MyPage from '../pages/mypage/MyPage'
+import LoginPage from '../pages/login/LoginPage'
 
-// 각 경로에 대응하는 페이지 컴포넌트 import
-import MainPage from "../pages/main/MainPage";
-import TimelinePage from "../pages/timeline/TimelinePage";
-import CommunityPage from "../pages/community/CommunityPage";
-import MyPage from "../pages/mypage/MyPage";
-import LoginPage from "../pages/login/LoginPage";
-import Header from "../widgets/header/Header";
-import { Outlet } from "react-router-dom";
-
-// path: 브라우저 주소, element: 그 주소에서 보여줄 컴포넌트
-// 배열 형태로 등록해두면 router가 알아서 주소 보고 매칭
-
-
-// 공통 레이아웃: Header 위에 고정
-// <Outlet />이 있는 자리에 현재 경로에 맞는 페이지 컴포넌트 들어감
+// 기존 페이지들이 공유하던 레이아웃 (Header + Outlet)
+// 홈 화면은 Navbar를 자체적으로 포함하고 있어서 이 레이아웃을 쓰지 않음
 function RootLayout() {
-    return (
-        <div>
-            <Header />
-            <Outlet />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  )
 }
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout />,
-        children: [
-            { index: true, element: <MainPage /> }, // index: '/'와 정확히 일치할 때
-            { path: 'timeline', element: <TimelinePage /> },
-            { path: 'community', element: <CommunityPage /> },
-            { path: 'mypage', element: <MyPage /> },
-            { path: 'login', element: <LoginPage /> },
-        ]
-    }
+  // 홈 화면: 독립된 라우트. Header 공통 레이아웃을 거치지 않고
+  // HomePage 안에서 Navbar를 직접 렌더링함
+  { path: '/', element: <HomePage /> },
+
+  // 나머지 페이지들: 기존처럼 Header + Outlet 레이아웃 공유
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      // 기존 '/'(지도)가 여기로 이동. travelId는 지금 하드코딩된 값만 들어옴
+      { path: 'travels/:travelId/map', element: <MainPage /> },
+      { path: 'timeline', element: <TimelinePage /> },
+      { path: 'community', element: <CommunityPage /> },
+      { path: 'mypage', element: <MyPage /> },
+      { path: 'login', element: <LoginPage /> },
+    ],
+  },
 ])
