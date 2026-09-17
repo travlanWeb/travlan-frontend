becd01e210166d81803e9bd04199f0dea2a52f18
wuheuijeong <wuheuijeong@wuheuijeongui-iMac.local>
2026-09-17 10:05:20 +0900

    feat: authSlice name,profile 필드 추가 및 홈 화면 로그인 상태별 문구 변경 기능 추가

--- src/entities/auth/model/authSlice.ts
+++ src/entities/auth/model/authSlice.ts
@@ -5,6 +5,8 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const storedAccessToken = localStorage.getItem('accessToken')
 const storedRefreshToken = localStorage.getItem('refreshToken')
+const storedName = localStorage.getItem('name')
+const storedProfileImage = localStorage.getItem('profileImage')
 
 const authSlice = createSlice({
 
@@ -12,6 +14,8 @@ const authSlice = createSlice({
 
     initialState: {
         // 처음 상태 정의하기
+        name: storedName,
+        profileImage: storedProfileImage,
         accessToken: storedAccessToken,
         refreshToken: storedRefreshToken,
         isLoggedIn: !!storedAccessToken ? true : false,
@@ -21,9 +25,13 @@ const authSlice = createSlice({
         // 상태 바꾸는 함수 정의
         // 매개변수 이름 state, action 으로 고정되어있음
         logout: (state) => {
+            state.name = null
+            state.profileImage = null
             state.accessToken = null
             state.refreshToken = null
             state.isLoggedIn = false
+            localStorage.removeItem('name')
+            localStorage.removeItem('profileImage')
             localStorage.removeItem('accessToken')
             localStorage.removeItem('refreshToken')
             localStorage.removeItem('isLoggedIn')
@@ -31,9 +39,14 @@ const authSlice = createSlice({
         
         // action.payload 안에 내용물이 담겨있음
         login: (state, action) => {
+            const profileImageToStore = action.payload.profileImage ? action.payload.profileImage : ''
+            state.name = action.payload.name
+            state.profileImage = profileImageToStore
             state.accessToken = action.payload.accessToken
             state.refreshToken = action.payload.refreshToken
             state.isLoggedIn = true
+            localStorage.setItem('name', action.payload.name)
+            localStorage.setItem('profileImage', profileImageToStore)
             localStorage.setItem('accessToken', action.payload.accessToken)
             localStorage.setItem('refreshToken', action.payload.refreshToken)
             localStorage.setItem('isLoggedIn', 'true')
